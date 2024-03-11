10da9fc4c796255f5aab493abe8307088af68050
Marcel Jaworski <marcel.jaworski@ils-gmbh.net>
2024-03-11 15:51:38 +0100

    getAccounts

--- types.go
+++ types.go
@@ -20,7 +20,7 @@ type Account struct {
 
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(10000),
+		// ID:        rand.Intn(10000),
 		FirstName: firstName,
 		LastName:  lastName,
 		Number:    int64(rand.Intn(1000000)),
