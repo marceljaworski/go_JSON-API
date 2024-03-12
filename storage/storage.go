0895513c9e6b27e9cafb9a9c67cd1907fa3624e8
Marcel Jaworski <marcel.jaworski@ils-gmbh.net>
2024-03-12 17:06:24 +0100

    println deleted

--- storage/storage.go
+++ storage/storage.go
@@ -56,7 +56,7 @@ func (s *PostgresStore) CreateAccount(acc *Account) error {
 	query := `insert into account
 	(first_name, last_name, number, balance, created_at) 
 	values($1, $2, $3, $4, $5)`
-	resp, err := s.db.Query(
+	_, err := s.db.Query(
 		query,
 		acc.FirstName,
 		acc.LastName,
@@ -66,7 +66,7 @@ func (s *PostgresStore) CreateAccount(acc *Account) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(resp)
+
 	return nil
 }
 func (s *PostgresStore) UpdateAccount(*Account) error {
